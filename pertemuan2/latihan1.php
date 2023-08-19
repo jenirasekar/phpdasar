8234e29ddc9c6dc2895d8dcc5366b0edb6a86d42
Jenira Sekar <jenirasekar@gmail.com>
2023-08-19 22:08:38 +0700

    style: fix missing end of file, refactor: rename file

--- pertemuan2/latihan1.php
+++ pertemuan2/latihan1.php
@@ -4,19 +4,19 @@
 // while
 // do.. while
 // foreach : pengulangan khusus array
+ 
+for( $i = 0; $i < 5; $i++ ) {
+   echo"Hello world!";
+}
 
-// for( $i = 0; $i < 5; $i++ ) {
-//    echo"Hello world!";
-// }
-// $i = 10;
-// while( $i < 5 ) {
- //       echo "Hello world! <br>";
-// $i++;
-// }
-// $i = 10;
-// do {
-//    echo "HellO World! <br>";
-// $i++;
-// while ( $i < 5 );
+$i = 10;
+while( $i < 5 ) {
+       echo "Hello world! <br>";
+$i++;
+}
 
-
+$i = 10;
+do {
+   echo "Hello World! <br>";
+$i++;
+} while ( $i < 5 );
