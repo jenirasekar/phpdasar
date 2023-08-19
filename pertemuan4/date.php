f35f04d48390a54e211ae394646ffb151964dae2
Jenira Sekar <jenirasekar@gmail.com>
2023-08-19 22:21:51 +0700

    style: add string concat

--- pertemuan4/date.php
+++ pertemuan4/date.php
@@ -1,20 +1,20 @@
 <?php  
 // Date
 // Untuk menampilkan tanggal dengan format tertentu
-//      echo date("l, d-M-Y");
+     echo date("l, d-M-Y");
 
-// // Time
-// // UNIX Timestamp / EPOCH Time
-// // detik yang sudah berlalu sejak 1 Januari 1970
-//      echo date("l" time()+60*60*24*23);
+// Time
+// UNIX Timestamp / EPOCH Time
+// detik yang sudah berlalu sejak 1 Januari 1970
+     echo date("l" . time()+60*60*24*23);
 
-// //  mktime
-// // membuat sendiri detik
-// // mktime(0, 0, 0, 0, 0, 0)
-// // jam, menit, detik, bulan, tanggal, tahun
-// echo mktime(0,0,0,9,7,2006);
+//  mktime
+// membuat sendiri detik
+// mktime(0, 0, 0, 0, 0, 0)
+// jam, menit, detik, bulan, tanggal, tahun
+echo mktime(0,0,0,9,7,2006);
 
-// // strtotime
-// echo date (strtotime("7 sep 2006"));
+// strtotime
+echo date (strtotime("7 sep 2006"));
 
 ?>
