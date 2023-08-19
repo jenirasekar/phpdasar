dba0b70d0d08ad169912d114bbad2bd0aad6b34a
Jenira Sekar <jenirasekar@gmail.com>
2023-08-19 22:42:40 +0700

    refactor: key value for img, style: add style to adjust img, edit list, and fix end of file

--- pertemuan6/latihan2.php
+++ pertemuan6/latihan2.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 // $mahasiswa = [["Jenira Sekar", "123456", "skar4289@gmail.com", "Teknik Informasi"],
 // 	["Rina Agustisya", "123457", "ngikngok@gmail.com", "Teknik Informasi"]
 // ];
@@ -8,36 +8,41 @@
 // key-nya adalah string yang kita buat sendiri
 $mahasiswa = [
 	[
-		"nama" = "Jenira Sekar";
-		"nrp" = "123456";
-		"email" = "skar4289@gmail.com";
-		"jurusan" = "Teknik Informatika";
+		"gambar" => "logo smk.jpg",
+		"nama" => "Jenira Sekar",
+		"nrp" => "123456",
+		"email" => "skar4289@gmail.com",
+		"jurusan" => "Teknik Informatika",
 	],
 	[
-		"nama" = "Rina Agustisya";
-		"nrp" = "123457";
-		"email" = "gakdaemail@gmail.com";
-		"jurusan" = "Teknik Informatika";
+		"gambar" => "logo smk.jpg",
+		"nama" => "Rina Agustisya",
+		"nrp" => "123457",
+		"email" => "gakdaemail@gmail.com",
+		"jurusan" => "Teknik Informatika",
 	]
 ];
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Daftar Mahasiswa</title>
 </head>
+
 <body>
 	<h1>Daftar Mahasiswa</h1>
 	<?php foreach ($mahasiswa as $mhs) : ?>
 		<ul>
 			<li>
-				<img src="img/<?= $mhs["gambar"] ?>">
+				<img src="img/<?= $mhs['gambar'] ?>" style="width: 100px;">
 			</li>
 			<li>Nama : <?= $mhs["nama"] ?></li>
-			<li>Nama : <?= $mhs["nrp"] ?></li>
-			<li>Nama : <?= $mhs["email"] ?></li>
-			<li>Nama : <?= $mhs["jurusan"] ?></li>
+			<li>NRP : <?= $mhs["nrp"] ?></li>
+			<li>Email : <?= $mhs["email"] ?></li>
+			<li>Jurusan : <?= $mhs["jurusan"] ?></li>
 		</ul>
-		<?php endforach; ?>
+	<?php endforeach; ?>
 </body>
+
 </html>
